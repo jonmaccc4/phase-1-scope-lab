675cffd4eeb2c19835d4aa9e94ce55ed062d087b
jondev <jonmac2454@gmail.com>
2025-03-08 17:59:34 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+// Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+
+const leastFavoriteCustomer = 'some initial value';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value'; 
+}
